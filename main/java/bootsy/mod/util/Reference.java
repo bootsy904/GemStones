26c574abe10107e8fd05c199fce143f19a78e51e
Bootsy <Bootsy904@hotmail.com>
2019-01-23 08:37:35 -0800

    Setup and Organization

---
+++ main/java/bootsy/mod/util/Reference.java
@@ -0,0 +1,13 @@
+package bootsy.mod.util;
+
+public class Reference {
+
+	public static final String MOD_ID = "bootsygsm";
+	public static final String NAME = "GemStones Mod";
+	
+	public static final String VERSION = "Alpha 1.0";
+	public static final String ACCEPTED_VERSIONS = "[1.12.1]";
+	
+	public static final String CLIENT = "bootsy.mod.proxy.ClientProxy";
+	public static final String COMMON = "bootsy.mod.proxy.CommonProxy";
+}
